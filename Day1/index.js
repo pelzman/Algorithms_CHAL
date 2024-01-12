97b77982eda0b3ab1f68991499e03d9b5bbbf01b
pelzman <oluwapelumiadeyemo72@gmail.com>
2024-01-12 09:52:14 +0100

    add Day5

--- Day1/index.js
+++ Day1/index.js
@@ -31,26 +31,12 @@ For the equal elements a[1] and b[1], no points are earned.
 Finally, for elements 2, a[2] > b[2] so Alice receives a point.
 The return array is [1, 1] with Alice's score first and Bob's second.
 
-Function Description
-
-Complete the function compareTriplets in the editor below.
-
-compareTriplets has the following parameter(s):
-
-int a[3]: Alice's challenge rating
-int b[3]: Bob's challenge rating
 Return
 
 int[2]: Alice's score is in the first position, and Bob's score is in the second.
 Input Format
 
-The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
-The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
 
-Constraints
-
-1 ≤ a[i] ≤ 100
-1 ≤ b[i] ≤ 100
 Sample Input 0
 
 5 6 7
---
+++ Day5/index.js
@@ -0,0 +1,50 @@
+function timeConversion(s) {
+    const [time, modifier] = s.split(' ');
+    let [hour, min, sec] = time.split(':');
+    if (hour === '12') {
+        hour = (modifier === 'AM') ? '00' : '12';
+        console.log(typeof hour)        
+    } else {
+        hour = (modifier === 'PM') ? String(parseInt(hour, 10) + 12) : hour;
+    }
+    return `${hour}:${min}:${sec}`;
+       console.log(typeof result  ) 
+    return result;
+}
+console.log(timeConversion("1:05:45 PM"))
+/*
+HackerLand University has the following grading policy:
+
+Every student receives a  in the inclusive range from  to .
+Any grade less than 40 is a failing grade.
+Sam is a professor at the university and likes to round each student's  according to these rules:
+
+If the difference between the grade  and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+If the value of grade  is less than 38 , no rounding occurs as the result will still be a failing grade.
+
+Examples
+
+ grade = 84 round to  (85 - 84 is less than 3)
+ grade = 29 do not round (result is less than 40)
+grade = 57  do not round (60 - 57 is 3 or higher)
+Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.
+*/
+
+
+function gradingStudents(grades) {
+ const grade = []   
+for(let i = 0; i < grades.length; i++){
+const originaValue = parseInt(grades[i])
+const ValueToNextMultipleOf5 = Math.ceil(originaValue/5)*5
+const diffValue = ValueToNextMultipleOf5 - originaValue
+if (originaValue >= 38 && diffValue < 3){
+    grade.push(ValueToNextMultipleOf5)
+}
+else {
+     grade.push(originaValue)
+}
+
+}
+return grade
+}
+console.log(gradingStudents([30, 73, 80,38]))
